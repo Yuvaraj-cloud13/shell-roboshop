9963a4299669d63859f2782636000a69ed693b77
yuvaraj <yuvarajhv1311@gmail.com>
2026-05-26 23:58:31 +0530

    shell script mongodb

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z00746892H2TX4JV63UWM"
+DOMAIN_NAME="uvraj.online"
+
+for instance in $@
+do
+    echo "Launching instance: $instance"
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
+    )
+    echo "Instance ID: $INSTANCE_ID"
+done
